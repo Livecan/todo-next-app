fc283c54460425fec963211e770e7811f39db9c4
Livecan <roman.meszaros@gmail.com>
2023-02-24 14:34:16 +0200

    ADD Simple home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
-import ListViewContainer from "@/src/containers/listViewContainer";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function Home() {
-  return <ListViewContainer id={"12"} />;
+  return (
+    <Box display="flex" flexDirection="column">
+      <Typography variant="h1">Roman&apos;s @todo App</Typography>
+      <Typography>
+        You can continue browsing through @todo lists by clicking on the menu
+        icon in the top left corner.
+      </Typography>
+    </Box>
+  );
 }
